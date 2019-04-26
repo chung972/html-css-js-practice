bf1d1f05a4fda74548a3c40630f6293ef3cfb9df
Albert Chung <chung972@gmail.com>
2019-04-26 10:10:05 -0500

    renamed irelia img with correcnt convention

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-alert("loaded");
+// alert("loaded");
 // just a reminder, this is a good little test to make sure your JS is 
 // hooked up to your html. comment out (or delete, which is preferred)
